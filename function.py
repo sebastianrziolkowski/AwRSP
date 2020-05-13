4cd8aed93eb2350a8ebb515f04a3915e76b40561
Sebastian Ziolkowski <osomjokey2@gmail.com>
2020-05-13 02:20:36 +0200

    function.py (1-13)

---
+++ function.py
@@ -0,0 +1,151 @@
+import math
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        print("You can't divide by 0!")
+        return 0
+    else:
+        return a / b
+
+
+def platon(n):
+    if 0 <= n < 10:
+        result = math.factorial(n)
+        if n < 7:
+            print("Not enough inhabitants: " + str(result))
+        elif n > 7:
+            print("Too many inhabitants: " + str(result))
+        else:
+            print("Perfect!  " + str(result))
+    else:
+        print("Input out of range!")
+
+
+def pythagoras(a, b, c):
+    sideLengths = [a, b, c]
+    sideLengths.sort()
+    result = sideLengths[0] ** 2 + sideLengths[1] ** 2
+    if result == sideLengths[2] ** 2:
+        print('This is pythagoras triangle. ' + str(sideLengths))
+    else:
+        print('This isn`t pythagoras triangle. ' + str(sideLengths))
+
+
+def login(inputPassword):
+    defaultPassword = "admin123"
+    resultList = []
+    if inputPassword == defaultPassword:
+        print("Password correct")
+    else:
+        for char in inputPassword:
+            if defaultPassword.find(char) == -1:
+                resultList.append(char)
+        print("Wrong password! This characters not needed: " + str(resultList))
+
+
+def eur_to_pln(a=1):
+    driver = webdriver.Chrome("E:\\Python\\chromedriver.exe")
+    driver.get(
+        "https://www.google.com/search?q=euro+kurs&oq=euro+kurs&aqs=chrome..69i57j0l7.3169j1j7&sourceid=chrome&ie=UTF-8")
+    content = driver.page_source
+    soup = BeautifulSoup(content, 'html.parser')
+    start_cut = "data-value="
+    end_cut = ">"
+    euro_exchange_rate = soup.find_all('span', class_='DFlfde SwHCTb')
+    print(a * float(str(euro_exchange_rate)[
+                    str(euro_exchange_rate).index(start_cut) + len(start_cut) + 1:str(euro_exchange_rate).index(
+                        end_cut) - 1]))
+
+
+def BMI(weight, hight):
+    return round(weight / (hight / 100) ** 2, 2)
+
+
+def exercise_7(a, b):
+    result = 0
+    for x in range(a ** b):
+        result = result + x
+    return result
+
+
+def exam_result(points):
+    if 0 <= points <= 100:
+        if points <= 50:
+            print("Result: 2")
+        elif points <= 70:
+            print("Result: 3")
+        elif points <= 90:
+            print("Result: 4")
+        else:
+            print("Result: 5")
+    else:
+        print("Points amount out of range!")
+
+
+def show_number(a, b, c, parameter):
+    number_list = [a, b, c]
+    number_list.sort()
+    if parameter == "DESC":
+        print(str(number_list))
+    elif parameter == "ASC":
+        number_list.reverse()
+        print(str(number_list))
+    else:
+        print("Wrong parameter!")
+
+
+def PKP(km):
+    if 0 < km <= 10:
+        return 20
+    elif 11 <= km <= 30:
+        return 10 + km * 0.1
+    elif 30 < km:
+        return 1 + 0.08 * km
+    else:
+        return 0
+
+
+def office_open_time(day):
+    switcher = {
+        1: "Pn: 10 – 14",
+        2: "Wt: 10 – 19",
+        3: "Śr – Pt: 11 – 16",
+        4: "Śr – Pt: 11 – 16",
+        5: "Śr – Pt: 11 – 16",
+        6: "So – Nd: Nieczynne",
+        7: "So – Nd: Nieczynne",
+    }
+    print(switcher.get(day, "Invalid day"))
+
+
+def exercise_12(n):
+    list_of_number = []
+    for x in range(n):
+        list_of_number.append(x + 1)
+    print("Elements: " + str(list_of_number))
+    print("Add: " + str(sum(list_of_number)))
+
+
+def exercise_13(string, char):
+    result = 0
+    for element in string:
+        if element == char:
+            result = result + 1
+    return result
